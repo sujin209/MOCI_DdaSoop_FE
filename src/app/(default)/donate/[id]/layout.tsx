6a9c230f6ea321d41658e04bac63466d10be3523
박수진 <qkrtnwls00000000@gmail.com>
2026-05-17 14:39:59 +0900

    [Fix]후원SEO추가 및 AI리뷰도입

--- src/app/(default)/donate/[id]/layout.tsx
+++ src/app/(default)/donate/[id]/layout.tsx
@@ -1,6 +1,49 @@
-import DonateDetailLayout from "@/domain/donate/components/DonationDetailLayout";
+import { getInitDonationDetail } from "@/domain/donate/api/getInitDonationDetail";
+import DonateDetailInfo from "@/domain/donate/components/DonateDetailInfo";
 import Loading from "@/domain/participation/components/DetailLoadingSkeleton";
+import ImageSwiper from "@/shared/components/ImageSwiper";
+import TabBar from "@/shared/components/TabBar";
+import { queryKeys } from "@/shared/config/queryKeys";
+import { donateTabContents } from "@/shared/utils/navigation";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+  const { id } = await params;
+
+  const data = await getInitDonationDetail(id);
+  const detail = data.data;
+
+  return {
+    title: `따숲 후원하기 | ${detail.title}`,
+    description: detail.description ?? "따숲 후원하기 상세 페이지",
+    keywords: ["후원", "후원하기", "목표달성", "커뮤니티"],
+    alternates: {
+      canonical: `https://www.ddasoop.xyz/donate/${id}`,
+    },
+    openGraph: {
+      title: `따숲 후원하기 | ${detail.title}`,
+      description: detail.description,
+      url: `https://www.ddasoop.xyz/donate/${id}`,
+      siteName: "따숲",
+      images: detail.imageUrls?.[0] ?? "",
+      locale: "ko_KR",
+      type: "website",
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: detail.title,
+      description: detail.description ?? "",
+      images: [detail.imageUrls?.[0] ?? ""],
+    },
+  };
+}
 
 async function page({
   params,
@@ -9,10 +52,40 @@ async function page({
   params: Promise<{ id: string }>;
   children: React.ReactNode;
 }) {
-  return (
-    <Suspense fallback={<Loading />}>
-      <DonateDetailLayout params={params}>{children}</DonateDetailLayout>
-    </Suspense>
+    const { id } = await params;
+  const queryClient = new QueryClient();
+
+  const detailInfo = await queryClient.fetchQuery({
+    queryKey: queryKeys.donate.id(id),
+    queryFn: () => getInitDonationDetail(id),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  
+    return (
+    <HydrationBoundary state={dehydratedState}>
+      <Suspense fallback={<Loading />}>
+        <div className="flex justify-between pt-4">
+          <section className="w-[calc(100%-280px)]">
+            <div className="w-full aspect-10/7">
+              <ImageSwiper
+                slideList={
+                  detailInfo.data.imageUrls?.map((img) => ({
+                    imageUrl: img,
+                  })) ?? []
+                }
+              />
+            </div>
+            <nav>
+              <TabBar type="donate" tabContents={donateTabContents(id)} />
+            </nav>
+            <main className="py-4">{children}</main>
+          </section>
+          <DonateDetailInfo id={id} />
+        </div>
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
