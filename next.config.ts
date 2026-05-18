735e50c4dd8493ef6896279fae626e991f6508ae
박수진 <qkrtnwls00000000@gmail.com>
2026-05-19 00:48:10 +0900

    [Fix]SEO개선

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 // TODO : 개발환경에서만 테스트 용도로 사용하고 수정할 것
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    // unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
